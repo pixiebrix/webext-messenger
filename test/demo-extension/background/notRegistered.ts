e36587e2262f5876bf3a902c675359f8857bd0d6
Federico <me@fregante.com>
2021-09-13 00:55:07 +0700

    Cleanup

--- test/demo-extension/background/notRegistered.ts
+++ test/demo-extension/background/notRegistered.ts
@@ -1,5 +1,3 @@
-// This file deliberately does not registed itself. It's only here for testing
-
 export async function notRegistered(): Promise<never> {
   throw new Error("This function should not have been registered");
 }
--- test/demo-extension/contentscript/getPageTitle.ts
+++ test/demo-extension/contentscript/getPageTitle.ts
@@ -1,6 +1,3 @@
 export async function getPageTitle(): Promise<string> {
-  console.log(window.parent === self, document.title);
   return document.title;
 }
-
-console.log(3);
