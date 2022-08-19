6e34e949691112cd371f37fc26e0f4c1b407b537
Federico Brigante <me@fregante.com>
2022-08-20 01:14:22 +0700

    Types

--- source/test/webextensionPolyfill.ts
+++ source/test/webextensionPolyfill.ts
@@ -1,3 +1,4 @@
 import browser from "webextension-polyfill";
 
+// @ts-expect-error Required until https://github.com/mozilla/webextension-polyfill/pull/376 or https://github.com/pixiebrix/webext-messenger/issues/67#issuecomment-1147121474
 globalThis.browser = browser;
