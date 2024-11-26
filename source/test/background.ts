d9157cb84e9b30a40ee7f9289afdc4ccdbe73632
fregante <me@fregante.com>
2024-11-26 15:32:01 +0700

    Don't automatically open the options because they steal OS focus

--- source/test/background.ts
+++ source/test/background.ts
@@ -1,8 +1,3 @@
 import "./webextensionPolyfill.js";
 import "./background/registration.ts";
 import "./contentscript/api.test.ts";
-import { onExtensionStart } from "webext-events";
-
-onExtensionStart.addListener(() => {
-  void browser.runtime.openOptionsPage();
-});
