465b7a0205023678820c65f0bfd4a3617891b6f9
Federico <me@fregante.com>
2024-11-28 13:00:38 +0700

    Silence `offscreen.createDocument`

--- source/test/background.ts
+++ source/test/background.ts
@@ -2,9 +2,20 @@ import "./webextensionPolyfill.js";
 import "./background/registration.ts";
 import "./contentscript/api.test.ts";
 
-void chrome.offscreen.createDocument({
-  url: "offscreen.html",
-  // @ts-expect-error wrong?
-  reasons: ["DOM_PARSER"],
-  justification: "testing",
-});
+async function init() {
+  try {
+    await chrome.offscreen.createDocument({
+      url: "offscreen.html",
+      // @ts-expect-error wrong?
+      reasons: ["DOM_PARSER"],
+      justification: "testing",
+    });
+  } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any
+    if (!(error as any).message.includes("Only a single offscreen")) {
+      throw error;
+    }
+  }
+}
+
+void init();
